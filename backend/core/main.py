20bd7d971fc8bcfed5e466b06653c0e5e8429674
Jasiel Serra <jasiel_serra@hotmail.com>
2017-08-13 11:55:53 -0300

    Redirect for home.

--- backend/core/main.py
+++ backend/core/main.py
@@ -3,9 +3,9 @@
 
 from __future__ import unicode_literals
 
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Flask!"
+    return redirect('/static/home.html')
