3f04d6838d7cb154f5763e58ae0cd06eed030298
Jasiel Serra <jasiel_serra@hotmail.com>
2017-08-27 12:04:17 -0300

    Add subscription_link in home and tests

--- backend/core/tests/test_main.py
+++ backend/core/tests/test_main.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import pytest
 
-
-def test_status_code(test_client):
+@pytest.fixture
+def home_resp(test_client):
     resp = test_client.get('/')
-    assert 302 == resp.status_code
+    return resp
+
+def test_status_code(home_resp):
+    assert 200 == home_resp.status_code
+
+def test_subscription_link(home_resp):
+    assert '/inscricao/' in home_resp.data.decode('utf8')
+
 
 '''
 def test_content():
